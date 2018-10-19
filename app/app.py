0ca92f8b98e2f23cbec7b68b7089a2195254966a
ks <mugoh.ks@gmail.com>
2018-10-19 12:02:15 +0300

    sales resourcecreated

--- app/app.py
+++ app/app.py
@@ -1,46 +1,53 @@
 
-from flask import Flask, jsonify
+from flask import Flask, abort
 
+my_app = Flask(__name__, static_url_path="")
 
 sales = [
     {
         'sales_record': 1,
         'attendant': u'Attendant One',
+
+        # Customer contacts
+        'name': u'Customer One',
+        'address': u'45 bright street',
+        'contact': [u'+00012345', u'customer_c@example.co'],
+
+        # Transaction Info
         'product': u'Spam 2.0',
         'quantity': u'1',
-        'complete': False,
+        'date': datetime(2018, 6, 6, 5, 28, 56, 243),
         'description': u'Hot with extra extra spam',
-        'customer': {
-            'name': u'Customer One',
-            'address': u'45 bright street',
-            'contact': [u'+00012345',
-                        u'customer_one@example.co'
-                        ]
-        },
-        'transaction type': u'Cash on Delivery',
-        'gifts': None,
-        'total for this sale': u'Ksh 276'
+        'transaction_type': u'Cash on Delivery',
+        'complete': False,
+
+        'gifts': 1,  # Anything to reduce sale e.g discounts
+        'total': 276
     },
 
     {
         'sales_record': 2,
         'attendant': u'Attendant Six',
-        'product': u'Spam 2.0',
+
+        # Customer contacts
+        'name': u'Customer fifty',
+        'address': u'42 bright street',
+        'contact': [u'+00012345', u'customer_c@example.co'],
+
+        # Transaction Info
+        'product': u'Spam 2.2',
         'quantity': u'1',
-        'complete': False,
+        'date': datetime(2018, 3, 16, 10, 3, 10, 7345),
         'description': u'Black with red eatable margins',
-        'customer': {
-            'name': u'Customer fifty',
-            'address': u'42 bright street',
-            'contact': [u'+00012345',
-                        u'customer_c@example.co'
-                        ]
-        },
-        'transaction type': u'Credit Card',
-        'gifts':None,
-        'total for this sale': u'Ksh 355'
+        'transaction_type': u'Credit',
+        'complete': False,
 
-
+        'gifts': 0,  # Any reduction in sale price
+        'total': 355
     }
 
 ]
+
+
+
+
