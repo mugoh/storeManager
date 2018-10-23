130177323b488ad95fb3b15473b2ea6dae536939
root <root@localhost.localdomain>
2018-10-23 17:12:28 +0300

    bug [#161416449] Solve for module importation errors

---
+++ app/api/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, Blueprint
+from flask_restful import Resource, Api
+from views import product_views, sale_views
+
+store_blueprint = Blueprint("store-man", __name__)
+
+
+def create_app():
+    my_app = Flask(__name__)
+    api = Api(store_blueprint)
+
+    api.add_resource(sale_views.SalesAPI,
+                     '/stman/api/v1.0/sales', endpoint='sales')
+    api.add_resource(product_views.ProductsAPI,
+                     '/stman/api/v1.0/products', endpoint='products')
+
+    my_app.register_blueprint(store_blueprint)
+    return my_app
