46f2f9eb8df4a3a83a4bbc53bb0ed828a4b8c3be
root <root@localhost.localdomain>
2018-10-23 17:00:09 +0300

    feature [#161416118] configure app to run

---
+++ app/api/__init__.py
@@ -0,0 +1,4 @@
+from app import create_app
+
+def start_app():
+	return create_app()
--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from app import app
+from api import start_app
 
 if __name__ == '__main__':
-	app.my_app.run()
+	start_app.run()
