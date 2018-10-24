7d8a4912765167b76dba24493cf36a5151a785f7
root <root@localhost.localdomain>
2018-10-25 02:37:09 +0300

    feature [#161350673] Give actionable uri error message

--- app/api/v1/views/productv.py
+++ app/api/v1/views/productv.py
@@ -1,5 +1,4 @@
-from flask import abort
-from flask_restful import Resource, reqparse, fields, marshal_with
+from flask_restful import Resource, reqparse, abort, fields, marshal_with
 import datetime
 from app.api.models.products import Products
 
@@ -40,8 +39,7 @@ class ProductAPI(Resource):
         if product_id not in record_instance.product_records:
             reply = f'Product {product_id} unknown. Maybe create it?'
 
-            return {
-                'message': reply}
+            abort(404,message=reply)
 
     @marshal_with(product_fields)
     def get(self, id):
