34606a7aa70b354aed02205ee7db6a94e12d2c5e
root <root@localhost.localdomain>
2018-10-24 06:11:29 +0300

    chore [#161437785] Resolve for environment configuration

--- app/api/app.py
+++ app/api/app.py
@@ -1,12 +1,14 @@
 from flask import Flask, Blueprint
 from flask_restful import Api
 from views import product_views, sale_views
+from instance.config import app_config
 
 store_blueprint = Blueprint("store-man", __name__)
 
 
-def create_app():
+def create_app(config_setting):
     my_app = Flask(__name__)
+    my_app.config.from_object(app_config[config_setting])
     api = Api(store_blueprint)
 
     api.add_resource(sale_views.SalesAPI,
---
+++ app/api/run.py
@@ -0,0 +1,4 @@
+import start_app
+
+if __name__ == '__main__':
+	start_app.run()
