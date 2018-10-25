fdfdf5bb85a13db304bba133c58c36f7fb0b58aa
root <root@localhost.localdomain>
2018-10-25 09:17:42 +0300

    chore [#161338640]implement class usage in user model

---
+++ app/api/v1/models/users.py
@@ -0,0 +1,8 @@
+
+class Users():
+    def __init__(self, name, username, email, password):
+        self.name = name
+        self.username = username
+        self.email = email
+        self.password = password
+        self.id = 0
