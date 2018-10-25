e840ea4f7349ddcbecd95b02c592810189b82540
root <root@localhost.localdomain>
2018-10-25 07:36:31 +0300

    feature [#161413576] create a model for the sales

---
+++ app/api/v1/models/sales.py
@@ -0,0 +1,14 @@
+
+class Sales:
+    def __init__(self,
+                 attendant, product, customer_name, quantity, price, date,
+                 transaction_type):
+
+        self.attendant = attendant
+        self.customer_name = customer_name
+        self.product = product
+        self.price = price
+        self.quantity = quantity
+        self.date = date
+        self.transaction_type = transaction_type
+        self.sale_record = 0
---
+++ app/api/v1/views/salesv.py
@@ -0,0 +1,3 @@
+from flask_restful import Resource, reqparse, abort, fields, marshal_with
+import datetime
+from app.api.v1.models.sales import sales
