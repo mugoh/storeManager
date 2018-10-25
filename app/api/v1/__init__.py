0cdcb066758cbb386d8cf9e19d03b2cf020e9f5d
root <root@localhost.localdomain>
2018-10-25 08:23:34 +0300

    feature [#161336043] Add get-all-sales option for user

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, Flask
 from flask_restful import Api
-from app.api.v1.views import productv
+from app.api.v1.views import productv, salesv
 
 app = Flask(__name__)
 v1 = Blueprint('api', __name__, url_prefix='/api/v1.0')
@@ -8,6 +8,8 @@ api = Api(v1)
 
 api.add_resource(productv.ProductList, '/products')
 api.add_resource(productv.ProductAPI, '/products/<int:id>')
+api.add_resource(salesv.SaleAPI, '/sales/<int:id>')
+api.add_resource(salesv.SalesList, '/sales')
 
 
 app.register_blueprint(v1)
--- app/api/v1/views/salesv.py
+++ app/api/v1/views/salesv.py
@@ -1,6 +1,6 @@
 from flask_restful import Resource, reqparse, abort, fields, marshal_with
 import datetime
-from app.api.v1.models.sales import sales
+from app.api.v1.models.sales import Sales
 
 
 class InitializeRecord:
@@ -23,6 +23,16 @@ class InitializeRecord:
 
 sale_instance = InitializeRecord()
 
+sale_fields = {
+    'attendant': fields.String,
+    'customer_name': fields.Integer,
+    'product': fields.String,
+    'quantity': fields.Integer,
+    'date': fields.DateTime,
+    'transaction_type': fields.String,
+    'price': fields.Integer,
+}
+
 
 class SaleAPI(Resource):
     @staticmethod
@@ -39,14 +49,58 @@ class SaleAPI(Resource):
         return sale_instance.fetch_record(sale_record)
 
 
+class SalesList(Resource):
+    @marshal_with(sale_fields)
+    def get(self):
+        all = [sale for sale
+               in sale_instance.sale_records.values()]
 
-sale_fields = {
-    'attendant': fields.String,
-    'customer_name': fields.Integer,
-    'product': fields.String,
-    'quantity': fields.Integer,
-    'date': fields.DateTime,
-    'transaction_type': fields.String,
-    'price': fields.Integer,
-    'total': fields.Integer
-}
+        return all
+
+    @marshal_with(sale_fields)
+    def post(self):
+        self.parse = reqparse.RequestParser()
+        self.parse.add_argument('attendant', type=str,
+                                required=True,
+                                help="A sale need's an attendant",
+                                location='json')
+
+        # Customer details
+        self.parse.add_argument('customer_name', type=str,
+                                default='Anonymous',
+                                location='json')
+
+        self.parse.add_argument('product', type=str,
+                                required=True,
+                                help="A product to sell sure has a name",
+                                location='json')
+
+        self.parse.add_argument('quantity', type=int,
+                                help="How many items", default=1,
+                                location='json')
+
+        self.parse.add_argument('transaction_type', type=str,
+                                default='Cash on Delivery',
+                                location='json')
+
+        self.parse.add_argument('price', type=int, required=True,
+                                help="""You sure are not giving it away for free
+                                """,
+                                location='json')
+
+        elements = self.parse.parse_args()
+
+        new_sale = Sales(
+            product=elements['product'],
+            customer_name=elements['customer_name'],
+            price=elements['price'],
+            quantity=elements['quantity'],
+            transaction_type=elements['transaction_type'],
+            attendant=elements['attendant'],
+            date=datetime.datetime.now()
+        )
+        sale_instance.post_record(new_sale)
+
+        return new_sale, 201
+
+
